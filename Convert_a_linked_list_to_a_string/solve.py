34fde9651cad903fee45fff000c0e731a6ece159
BodiyaKol <kolodchak.pn@ucu.edu.ua>
2025-04-04 19:14:46 +0300

    Update solve.py
    solving problem current = next.next

--- Convert_a_linked_list_to_a_string/solve.py
+++ Convert_a_linked_list_to_a_string/solve.py
@@ -1,14 +1,6 @@
 '''
 Convert a linked list to a string
 '''
-class Node:
-    '''
-    A node of a linked list.
-    '''
-    def __init__(self, data, next_node=None):
-        self.data = data
-        self.next = next_node
-
 def stringify(node):
     '''
     Convert a linked list to a string representation.
@@ -19,6 +11,6 @@ def stringify(node):
     current = node.next
     while current:
         values_list.append(str(current.data))
-        current = next.next
+        current = current.next
     values_list.append('None')
     return ' -> '.join(values_list)
